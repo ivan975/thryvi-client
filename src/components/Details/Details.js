66d73d775a56e26b0e4316ae921a3d7e4176dd38
= <rakib.ivan100@gmail.com>
2022-10-25 20:07:55 +0600

    dynamic link created

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,8 +5,19 @@ const Details = () => {
     const details = useLoaderData();
     console.log(details);
     return (
-        <div>
-            <p>{details.title}</p>
+        <div className=''>
+            <div className="flex justify-center mt-16 mb-9">
+                <div className="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={details.image} alt="country" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-2xl font-bold">{details.title}</h2>
+                        <p className='text-start'>{details.description}</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary mr-5">Get Visa</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
