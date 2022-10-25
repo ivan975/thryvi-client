e44aa3d220886662665bde7426dbb166bebad745
= <rakib.ivan100@gmail.com>
2022-10-25 15:21:56 +0600

    google and github signin done

--- src/components/Contexts/UserContext.js
+++ src/components/Contexts/UserContext.js
@@ -1,14 +1,30 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { app } from '../../firebase/firebase.init';
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 const UserContext = ({ children }) => {
+    const [user, setUser] = useState('');
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleSignIn = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const githubSignIn = () => {
+        return signInWithPopup(auth, githubProvider)
+    }
+
+    const authInfo = { user, createUser, googleSignIn, githubSignIn }
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Contexts/UserContext';
 
 const Login = () => {
+    const { googleSignIn, githubSignIn } = useContext(AuthContext);
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col mb-52">
