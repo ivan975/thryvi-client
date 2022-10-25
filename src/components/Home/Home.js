6abab0a8a821b450e7a3116d046b46642f161f2d
= <rakib.ivan100@gmail.com>
2022-10-25 11:37:11 +0600

    home page done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import Lottie from 'lottie-react';
+import { Link } from 'react-router-dom';
+import home from '../../assests/home.json';
 const Home = () => {
     return (
         <div>
             <div className="hero min-h-screen">
-                <div className="hero-content flex-col lg:flex-row-reverse bg-rose-300 rounded-lg p-12 mb-14">
-                    
+                <div className="hero-content flex-col lg:flex-row-reverse bg-fuchsia-200 rounded-lg p-12 mb-52">
+                    <Lottie animationData={home}></Lottie>
                     <div>
                         <h1 className="text-5xl font-bold flex justify-start">Learn new technologies</h1>
                         <p className="py-6 text-start mr-52">This is one of the best platform to explore technologies and this is an non-profit organization.</p>
-                        <button className="btn btn-primary flex">Get Started</button>
+                        <Link to='/courses'><button className="btn btn-primary flex">Get Started</button></Link>
                     </div>
                 </div>
             </div>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../components/Navbar/Home/Home";
+import Home from "../components/Home/Home";
 import Main from "../layout/Main";
 
 export const router = createBrowserRouter([
